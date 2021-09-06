70ab957061769703e9ade0b7edf5c0405956b39e
Rakveerchand <rkchauhan3101@gmail.com>
2021-09-06 13:01:05 +0530

    Update shape.js

--- shape.js
+++ shape.js
@@ -1,3 +1,16 @@
+// export class Shape{
+//     constructor(color) {
+//     this.color = "black";
+//   }
+    
+// drawShape(){
+//     return "drawShape";
+// }
+// calculateArea(){
+//     return "calculate Area";
+// }
+// }
+
 export class Shape{
     constructor(color) {
     this.color = "black";
