e6b7e31860139bd24941bafac9930e9f14a165e1
Rakveerchand <rkchauhan3101@gmail.com>
2021-09-06 13:00:34 +0530

    Update circle.js

--- circle.js
+++ circle.js
@@ -1,9 +1,15 @@
-import { Shape } from "./shape";
+// import { Shape } from "./shape";
 
+// class Circle extends Shape {
+
+// calculateArea(){
+//     return "calculate area from child class";
+// }
+
+// }
+var Shape = require('./shape.js');
 class Circle extends Shape {
-
-calculateArea(){
-    return "calculate area from child class";
+    constructor() { super(); this.radius = 9; }
+    calculateArea() { return "Calculated area of circle"; }
 }
-
-}
+module.exports = Circle;
